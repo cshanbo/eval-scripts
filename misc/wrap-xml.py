71c083dcfcda6580d267724a32477c62b4fa7f56
Shanbo Cheng <cshanbO@gmail.com>
2017-05-22 10:07:38 +0800

    update wrap-xml.py

--- misc/wrap-xml.py
+++ misc/wrap-xml.py
@@ -6,16 +6,15 @@ import codecs
 import io
 
 def meth1():
-    if len(sys.argv) < 7:
-        sys.stderr.write('usage: %s + input.sl.sgm + plain-txt + sl + tl + add-xmlhead + output-file + sysid [optional]\n' % __file__)
+    if len(sys.argv) != 9:
+        sys.stderr.write('usage: %s + input.sl.sgm + plain-txt + sl + tl + add-xmlhead + output-file + site + sysid\n' % __file__)
         sys.exit(-1)
-
-    sysid = 'SOGOU'
-    if len(sys.argv) >= 8:
-        sysid = ' '.join(sys.argv[8:])
 
     sl = sys.argv[3]
     tl = sys.argv[4]
+    site = sys.argv[7]
+    sysid = sys.argv[8]
+    outputfile = sys.argv[6]
     output = []
 
     if sys.argv[5] != '0':
@@ -38,36 +37,27 @@ def meth1():
                     if trgl != tl:
                         sys.stderr.write('Target language not match: (%s and %s)\n' % (trgl, tl))
 
-                    # print '<tstset setid="%s" srclang="%s" trglang="%s">' % (setidfs[1].split('"')[1], srcl, trgl)
                     output.append('<tstset setid="%s" srclang="%s" trglang="%s">' % (setidfs[1].split('"')[1], srcl, trgl))
                 except:
                     sys.stderr.write('ERROR: setid, srclang or trglang NOT SET in original file! Using given srclang and trglang!!!\n')
-                    # print '<tstset setid="%s" srclang="%s" trglang="%s">' % (setidfs[1].split('"')[1], sl, tl)
                     output.append('<tstset setid="%s" srclang="%s" trglang="%s">' % (setidfs[1].split('"')[1], sl, tl))
-                # print '<system site="SOGOU" sysid="%s">' % sysid
-                # print '</system>'
-                output.append('<system site="SOGOU" sysid="%s">' % sysid)
+                output.append('<system site="%s" sysid="%s">' % (site, sysid))
                 output.append('</system>')
             elif line.startswith('</srcset'):
-                # print line.replace('srcset', 'tstset')
                 output.append(line.replace('srcset', 'tstset'))
             elif line.startswith('<seg id'):
                 pl = plain.readline().strip()
                 id = line.split('>')[0] + '>'
-                # print id + pl + '</seg>'
                 output.append(id + pl + '</seg>')
-            elif line.startswith('<doc'):
-                sysdd = line.split('sysid')[1].split('"')[1]
+            elif line.startswith('<doc') or line.startswith('<DOC'):
                 if 'sysid=' in line:
-                    # print line.replace(sysdd, sysid)
+                    sysdd = line.split('sysid')[1].split('"')[1]
                     output.append(line.replace(sysdd, sysid))
                 else:
-                    # print line[:-1] + ' sysid="%s"' % sysid + ">"
                     output.append(line[:-1] + ' sysid="%s"' % sysid + ">")
             else:
-                # print line
                 output.append(line)
-    with codecs.open(sys.argv[6], "w", "utf-8-sig") as ofile:
+    with codecs.open(outputfile, "w", "utf-8-sig") as ofile:
         for v in output:
             ofile.write(v.strip().decode('utf-8-sig') + '\n')
 
